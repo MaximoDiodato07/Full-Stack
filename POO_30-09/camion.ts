b57fff61e076ee4df170fbff12875238a9420d59
Maximo Diodato <166886271+MaximoDiodato07@users.noreply.github.com>
2024-09-30 18:41:04 -0300

    Update de comentarios

--- POO_30-09/camion.ts
+++ POO_30-09/camion.ts
@@ -7,7 +7,7 @@ private ruedas:number;
     
 constructor(color: string,rodado:number,marca:string,modelo:number,vtv:boolean, estado:boolean, ruedas:number) {
     super(color, rodado, marca, modelo, vtv, estado);
-    this.ruedas = this.setRuedas(ruedas); // inicializo siempre en 2
+    this.ruedas = this.setRuedas(ruedas); 
 }
 
 // En el caso del camion, las ruedas varian.
