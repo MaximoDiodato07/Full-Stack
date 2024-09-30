242d56b659c313f6399d9135e3311a2ab03bfeee
Maximo Diodato <diodatomaximo@gmail.com>
2024-09-30 18:37:48 -0300

    Cambios para metodos genericos, en edicion de vehiculos, y obtencion de los mismos.

--- POO_30-09/registroAutomor.ts
+++ POO_30-09/registroAutomor.ts
@@ -1,3 +1,4 @@
+import {automovil} from "./automovil"
 import { auto } from "./auto";
 import{ moto } from "./moto";
 import{ camion } from "./camion";
@@ -8,29 +9,60 @@ class registroAutomor{
     private moto:moto;
     private camion:camion;
     
-    // Pide los 3 tipos de vehiculos.
+    // Pide los 3 tipos de vehiculos. se podria hacer constructores distintos para c/u
     constructor(auto:auto,moto:moto,camion:camion){
         this.auto = auto;
         this.moto = moto;
         this.camion = camion;
     }
 
+    public modificarVehiculo(automovil:automovil, color?:string,rodado?:number){
+        // IF (variable) asegura que este inicializada
+        if(color) automovil.setColor(color);
+        if(rodado) automovil.setRodado(rodado);
+        return "Vehículo modificado exitosamente";
+    }
+
+    public getAuto():automovil{
+        return this.auto;
+    }
+
+    public getCamion():automovil{
+        return this.camion;
+    }
+
+    public getMoto():automovil{
+        return this.moto;
+    }
+
+
 }
 
-//--------- Variables esperadas (color, rodado, marca, modelo, vtv, estado)
-const auto1 = new auto("Rojo",14,"Mercedez Benz",2020,true);
+//--------- Variables esperadas (color, rodado, marca, modelo, vtv)
+const auto1 = new auto("Rojo", 14,"Mercedez Benz", 2020, true);
 
-const moto1 = new moto ("Blanco",19, "Motomel Skua",2013, true);
+const moto1 = new moto ("Blanco", 17, "Honda CB500X", 2023, false);
 
-//--------- Variables esperadas (color, rodado, marca, modelo, vtv, estado, ruedas)
-const camion1 = new camion("Ocre", 295, "fiat 1114", 1986, true, true, 8);
+//--------- Variables esperadas (color, rodado, marca, modelo, vtv, ruedas)
+const camion1 = new camion("Ocre", 22.5, "Volvo FH16", 2022, true, true, 6);
 
-console.log(camion1)
-// Modifico un camion
-camion1.setColor("Gris")
-camion1.setRuedas(12)
-console.log(camion1)
+console.log("Sin VTV")
+console.log(moto1);
+// Modifico una moto desde su metodo de clase.
+moto1.setVtv(true)
+console.log(moto1)
+
+// Creo registro.
 const registro = new registroAutomor(auto1,moto1,camion1);
 
+
+// Obtengo vehiculo a cambiar.
+let vehiculoModificable = registro.getCamion();
+
+console.log(vehiculoModificable); // Muestro estado incializado
+// Modifico un camion, le cambio el color y el rodado.
+registro.modificarVehiculo(vehiculoModificable, "Azul", 21)
+
+// de esta manera, podria modificar el color o el rodado de cualquier vehiculo.
 
 console.log(registro);
